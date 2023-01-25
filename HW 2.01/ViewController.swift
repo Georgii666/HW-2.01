168017a2b6b2f5f779e30b3ccc9f6f3611e48a6c
Georgii Litvinenko <jyrki.ss@gmail.com>
2023-01-25 21:41:56 +0200

    Initial Commit

---
+++ HW 2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.01
+//
+//  Created by GEORGII LITVINENKO on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
